41b23f2ae685c869ce30b87e5afb0ce0e80a95b4
Jack Wild <jack.wild@live.co.uk>
2014-06-02 14:54:16 +0200

    setup html, started AudioAnalysisEngine to handle setup of audio / mic

--- CS/AudioAnalysisEngine.coffee
+++ CS/AudioAnalysisEngine.coffee
@@ -1,1 +1,56 @@
-#Audio Analysis
+#Audio Analysis Engine
+$ ->
+	audioAnalysisEngine = new AudioAnalysisEngine();
+
+
+class AudioAnalysisEngine
+	_context: null
+	_source: null
+	_testAudio: null
+	_alreadySetup: false
+
+
+	constructor: ->
+		@_context = new webkitAudioContext()
+		@setupAnalyser()
+
+		@_testAudio = document.getElementById('test_audio')
+		# document.getElementById('magic').onclick = => @setupTestAudio()
+		document.getElementById('magic').onclick = =>
+			navigator.webkitGetUserMedia
+				audio: true
+			,@setupMic, @onError
+
+
+
+	setupAnalyser: =>
+		@_analyserNode = @_context.createAnalyser()
+		@_analyserNode.fftSide = 32
+		
+
+	setupTestAudio: =>
+		console.log 'setup test audio', @_testAudio
+		if (@_alreadySetup)
+			return
+		@_source = @_context.createMediaElementSource(@_testAudio)
+		@_source.connect @_analyserNode
+		@_analyserNode.connect @_context.destination
+		@_testAudio.play()
+		@startAnalysis()
+		@_alreadySetup = true
+
+	setupMic: (stream) =>
+		console.log 'setup mic'
+		if (@_alreadySetup)
+			return
+		@_source = @_context.createMediaStreamSource(stream)
+		@_source.connect @_analyserNode
+		@_analyserNode.connect @_context.destination
+		@startAnalysis()
+		@_alreadySetup = true
+
+	onError: (err) =>
+		console.log 'error setting up mic', err
+
+	startAnalysis: =>
+		console.log 'analysis started'
--- JS/AudioAnalysisEngine.js
+++ JS/AudioAnalysisEngine.js
@@ -1,4 +1,79 @@
 (function() {
+  var AudioAnalysisEngine,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
+  $(function() {
+    var audioAnalysisEngine;
+    return audioAnalysisEngine = new AudioAnalysisEngine();
+  });
+
+  AudioAnalysisEngine = (function() {
+    AudioAnalysisEngine.prototype._context = null;
+
+    AudioAnalysisEngine.prototype._source = null;
+
+    AudioAnalysisEngine.prototype._testAudio = null;
+
+    AudioAnalysisEngine.prototype._alreadySetup = false;
+
+    function AudioAnalysisEngine() {
+      this.startAnalysis = __bind(this.startAnalysis, this);
+      this.onError = __bind(this.onError, this);
+      this.setupMic = __bind(this.setupMic, this);
+      this.setupTestAudio = __bind(this.setupTestAudio, this);
+      this.setupAnalyser = __bind(this.setupAnalyser, this);
+      this._context = new webkitAudioContext();
+      this.setupAnalyser();
+      this._testAudio = document.getElementById('test_audio');
+      document.getElementById('magic').onclick = (function(_this) {
+        return function() {
+          return navigator.webkitGetUserMedia({
+            audio: true
+          }, _this.setupMic, _this.onError);
+        };
+      })(this);
+    }
+
+    AudioAnalysisEngine.prototype.setupAnalyser = function() {
+      this._analyserNode = this._context.createAnalyser();
+      return this._analyserNode.fftSide = 32;
+    };
+
+    AudioAnalysisEngine.prototype.setupTestAudio = function() {
+      console.log('setup test audio', this._testAudio);
+      if (this._alreadySetup) {
+        return;
+      }
+      this._source = this._context.createMediaElementSource(this._testAudio);
+      this._source.connect(this._analyserNode);
+      this._analyserNode.connect(this._context.destination);
+      this._testAudio.play();
+      this.startAnalysis();
+      return this._alreadySetup = true;
+    };
+
+    AudioAnalysisEngine.prototype.setupMic = function(stream) {
+      console.log('setup mic');
+      if (this._alreadySetup) {
+        return;
+      }
+      this._source = this._context.createMediaStreamSource(stream);
+      this._source.connect(this._analyserNode);
+      this._analyserNode.connect(this._context.destination);
+      this.startAnalysis();
+      return this._alreadySetup = true;
+    };
+
+    AudioAnalysisEngine.prototype.onError = function(err) {
+      return console.log('error setting up mic', err);
+    };
+
+    AudioAnalysisEngine.prototype.startAnalysis = function() {
+      return console.log('analysis started');
+    };
+
+    return AudioAnalysisEngine;
+
+  })();
 
 }).call(this);
