1e63bf04bfeeb00f53d5ab013f552a2df6817522
Jack Wild <jack.wild@live.co.uk>
2014-06-25 12:06:44 +0200

    server to JS

--- server.js
+++ server.js
@@ -1,56 +1,51 @@
-(function() {
-  var app, bodyParser, express, http, io, path, port, server, socket;
+var express = require('express');
 
-  express = require('express');
+var http = require('http');
 
-  http = require('http');
+var socket = require('socket.io');
 
-  socket = require('socket.io');
+var path = require('path');
 
-  path = require('path');
+var bodyParser = require('body-parser');
 
-  bodyParser = require('body-parser');
+var app = express();
 
-  app = express();
+var server = http.createServer(app);
 
-  server = http.createServer(app);
+var io = socket.listen(server);
 
-  io = socket.listen(server);
+app.get('/', function(request, response) {
+  var ua;
+  console.log('request sent');
+  ua = request.headers['user-agent'];
+  if (/mobile/i.test(ua)) {
+    console.log('mobile');
+    app.use(express["static"](path.join(__dirname, '')));
+    return response.sendfile(__dirname + "/mobile.html");
+  } else {
+    app.use(express["static"](path.join(__dirname, '')));
+    response.sendfile(__dirname + "/index.html");
+    return console.log('desktop');
+  }
+});
 
-  app.get('/', function(request, response) {
-    var ua;
-    console.log('request sent');
-    ua = request.headers['user-agent'];
-    if (/mobile/i.test(ua)) {
-      console.log('mobile');
-      app.use(express["static"](path.join(__dirname, '')));
-      return response.sendfile(__dirname + "/mobile.html");
-    } else {
-      app.use(express["static"](path.join(__dirname, '')));
-      response.sendfile(__dirname + "/index.html");
-      return console.log('desktop');
-    }
+var port = Number(process.env.PORT || 8080);
+
+io.sockets.on('connection', function(client) {
+  console.log('a client connected');
+  client.on('button-push', function(which) {
+    console.log(which);
+    return io.emit('button-push', which);
   });
-
-  port = Number(process.env.PORT || 8080);
-
-  io.sockets.on('connection', function(client) {
-    console.log('a client connected');
-    client.on('button-push', function(which) {
-      console.log(which);
-      return io.emit('button-push', which);
-    });
-    client.on('key-entered', function(which) {
-      console.log("key entered", which);
-      return io.emit('key-entered', which);
-    });
-    return client.on('disconnect', function(client) {
-      return console.log('a client disconnect');
-    });
+  client.on('key-entered', function(which) {
+    console.log("key entered", which);
+    return io.emit('key-entered', which);
   });
-
-  server.listen(port, function() {
-    return console.log('listening on ' + port);
+  return client.on('disconnect', function(client) {
+    return console.log('a client disconnect');
   });
+});
 
-}).call(this);
+server.listen(port, function() {
+  return console.log('listening on ' + port);
+});
